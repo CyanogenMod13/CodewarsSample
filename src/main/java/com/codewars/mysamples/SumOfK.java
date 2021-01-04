da8a8d8a6b2a0c74bcadbc81a43cae0c626fb545
CyanogenMod13 <sovkanbbb@gmail.com>
2021-01-04 20:02:05 +0200

    Uncompleted

--- src/main/java/com/codewars/mysamples/SumOfK.java
+++ src/main/java/com/codewars/mysamples/SumOfK.java
@@ -1,11 +1,11 @@
 //https://www.codewars.com/kata/55e7280b40e1c4a06d0000aa/train/java
 package com.codewars.mysamples;
 
-import com.codewars.mysamples.annotation.Uncompleted;
+import com.codewars.mysamples.annotation.UncompletedKata;
 
 import java.util.List;
 
-@Uncompleted
+@UncompletedKata
 public class SumOfK {
     public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
         if (ls.size() < k) return null;
